d5939493e29ff2a52ee479c477593bfa19133a4a
TARIK SARI <info@tariksari.com>
2020-10-17 22:17:28 +0300

    Update actions.ts

--- src/_store/modules/search/actions.ts
+++ src/_store/modules/search/actions.ts
@@ -5,18 +5,14 @@ import {types} from '../../types'
 
 export default {
 	[CONSTANTS.WORD_SEARCH_GET]: ({ commit, dispatch }:types, word:string) => {
-		return new Promise((resolve, reject) => {
 			commit(CONSTANTS.WORD_SEARCH_REQUEST);
 
 			Axios.get('search/' + word)
 				.then((resp) => {
-					console.log(resp);
 					commit(CONSTANTS.WORD_SEARCH_RESPONSE, resp);
 				})
 				.catch((err) => {
-					console.log(err);
 					commit(CONSTANTS.WORD_SEARCH_ERROR, err.status);
 				});
-		});
 	},
 };
