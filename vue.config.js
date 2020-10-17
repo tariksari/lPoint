7c5f740525a2c58b3f76f24ce38d36d4fabf8640
TARIK SARI <info@tariksari.com>
2020-10-18 01:31:13 +0300

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    pluginOptions: {
+        electronBuilder: {
+          builderOptions: {
+            "extraResources": [
+              {
+                "from": "./src/assets/app",
+                "to": "src/assets/app",
+                "filter": [
+                  "**/*"
+                ]
+              }
+            ]
+          },
+        }
+      },
+  }
