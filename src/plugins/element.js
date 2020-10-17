ee6c6a94119ab1774b7bc2b2cb17c6e899417e32
TARIK SARI <info@tariksari.com>
2020-10-17 22:14:15 +0300

    Delete element.js

--- src/plugins/element.js
+++
@@ -1,8 +0,0 @@
-import { Button } from 'element-ui';
-import locale from 'element-ui/lib/locale';
-import lang from 'element-ui/lib/locale/lang/en';
-import Vue from 'vue';
-
-locale.use(lang);
-
-Vue.use(Button);
