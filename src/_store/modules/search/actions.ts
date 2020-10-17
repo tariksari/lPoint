e45ef58a4a6e48a6ae4baae0e5e4dfa51d97a001
TARIK SARI <info@tariksari.com>
2020-10-17 22:27:27 +0300

    updated

--- src/_store/modules/search/actions.ts
+++ src/_store/modules/search/actions.ts
@@ -5,8 +5,6 @@ import {types} from '../../types'
 
 export default {
 	[CONSTANTS.WORD_SEARCH_GET]: ({ commit, dispatch }:types, word:string) => {
-			commit(CONSTANTS.WORD_SEARCH_REQUEST);
-
 			Axios.get('search/' + word)
 				.then((resp) => {
 					commit(CONSTANTS.WORD_SEARCH_RESPONSE, resp);
--- src/_store/modules/search/mutations.ts
+++ src/_store/modules/search/mutations.ts
@@ -1,13 +1,7 @@
 import * as CONSTANTS from './constants';
 
 export default {
-	[CONSTANTS.WORD_SEARCH_REQUEST]: (state:any) => {
-		//state.theme.dark_mode = !state.theme.dark_mode;
-	},
-	[CONSTANTS.WORD_SEARCH_RESPONSE]: (state:any, data:string) => {
+	[CONSTANTS.WORD_SEARCH_RESPONSE]: (state: any, data: string) => {
 		state.data = data;
-	},
-	[CONSTANTS.WORD_SEARCH_ERROR]: (state:any, data:string) => {
-		console.log(data);
-	},
+	}
 };
