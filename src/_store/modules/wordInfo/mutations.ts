5b8838f17bf6c05f77e9f630f58e256e424d40f2
TARIK SARI <info@tariksari.com>
2020-10-17 22:32:42 +0300

    Update mutations.ts

--- src/_store/modules/wordInfo/mutations.ts
+++ src/_store/modules/wordInfo/mutations.ts
@@ -1,13 +1,10 @@
 import * as CONSTANTS from './constants';
 
 export default {
-	[CONSTANTS.WORD_REQUEST]: (state:any) => {
-		//state.theme.dark_mode = !state.theme.dark_mode;
-	},
-	[CONSTANTS.WORD_RESPONSE]: (state:any, data:string) => {
+	[CONSTANTS.WORD_RESPONSE]: (state: any, data: string) => {
 		state.data = data;
 	},
-	[CONSTANTS.WORD_REQUEST_ERROR]: (state:any, data:string) => {
-		console.log(data);
+	[CONSTANTS.WORD_REQUEST_ERROR]: (state: any, err: string) => {
+		state.error = err;
 	},
 };
