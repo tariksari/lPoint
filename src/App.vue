0292b718d740190ce26e68a9bce2f120f736ce44
TARIK SARI <info@tariksari.com>
2020-10-17 22:15:10 +0300

    added loadBar

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,21 @@
     <router-link to="/word-list">WORD LIST</router-link>
     <router-link to="/search">SEARCH WORD</router-link>
   </div>
+  <load-bar></load-bar>
   <router-view />
 </template>
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+// @ts-ignore
+import LoadBar from "@/components/LoadBar";
+
+@Options({
+  components: {
+    LoadBar,
+  },
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="scss">
 @import "./assets/styles/main.scss";
