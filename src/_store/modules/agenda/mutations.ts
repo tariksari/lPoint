79543984cefd7183f83d01de6ab8b4780e446d7f
TARIK SARI <info@tariksari.com>
2020-10-17 22:32:50 +0300

    Update mutations.ts

--- src/_store/modules/agenda/mutations.ts
+++ src/_store/modules/agenda/mutations.ts
@@ -1,13 +1,16 @@
 import * as CONSTANTS from './constants';
 
 export default {
-	[CONSTANTS.AGENDA_REQUEST]: (state:any) => {
-		//state.theme.dark_mode = !state.theme.dark_mode;
-	},
 	[CONSTANTS.AGENDA_RESPONSE]: (state:any, data:string) => {
 		state.data = data;
 	},
-	[CONSTANTS.AGENDA_ERROR]: (state:any, data:string) => {
-		console.log(data);
+	[CONSTANTS.AGENDA_ERROR]: (state:any, err:string) => {
+		state.data = err;
+	},
+	[CONSTANTS.ADD_AGENDA_WORD_RESPONSE]: (state:any, data:string) => {
+		state.data = data;
+	},
+	[CONSTANTS.ADD_AGENDA_WORD_ERROR]: (state:any, err:string) => {
+		state.data = err;
 	},
 };
