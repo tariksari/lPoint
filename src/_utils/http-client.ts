070f9133ec2b47197010aa2a54c7cfbdd9d344e3
TARIK SARI <info@tariksari.com>
2020-10-17 22:14:46 +0300

    Update http-client.ts

--- src/_utils/http-client.ts
+++ src/_utils/http-client.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+import store from '../_store/index'
 
 import { API_CONFIG } from '@/_utils/api.config';
 
@@ -7,15 +8,29 @@ class Axios {
 
 	public constructor() {
 		this.instance = axios.create(API_CONFIG);
-
 		this.initializeResponseInterceptor();
 	}
 
 	private initializeResponseInterceptor = () => {
+		this.instance.interceptors.request.use(
+			(req) => {
+				 store.dispatch('APP/ACTION_LOADER_STATUS', true);
+				 return req;	
+			},
+			(error) =>{
+				store.dispatch('APP/ACTION_LOADER_STATUS', false);
+				return Promise.reject(error)
+			});
+
 		this.instance.interceptors.response.use(
-			({ data }: AxiosResponse) => data,
-			(error) => Promise.reject(error)
-		);
+			({ data }: AxiosResponse) => {
+				store.dispatch('APP/ACTION_LOADER_STATUS', false);
+				return data;
+			},
+			(error) =>{
+				store.dispatch('APP/ACTION_LOADER_STATUS', false);
+				return Promise.reject(error)
+			});
 	};
 }
 
