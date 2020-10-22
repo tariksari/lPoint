030bc4480e3e9f3025aced2bc6b576e73bc35d6a
TARIK SARI <info@tariksari.com>
2020-10-22 13:17:07 +0300

    fix item action box

--- src/views/Agenda.vue
+++ src/views/Agenda.vue
@@ -3,14 +3,22 @@
     <div :class="[viewType ? 'grid' : 'list', 'page agenda-list']">
       <div class="view-type-button" @click="this.listViewChanger()">
         <format-list-bulleted v-if="viewType"></format-list-bulleted>
-        <grid-large v-else></grid-large >
+        <grid-large v-else></grid-large>
       </div>
       <div v-for="(value, key) in this.wordData" :key="key.id" class="item">
-        <div class="audio">
-          <play-box-outline
-            @click="this.playAudio(value.audio)"
-          ></play-box-outline>
+        <div class="action">
+          <div class="box audio">
+            <play-box-outline
+              @click="this.playAudio(value.audio)"
+            ></play-box-outline>
+          </div>
+          <div class="box info">
+            <information-variant
+              @click="wordInfo(value.word)"
+            ></information-variant>
+          </div>
         </div>
+
         <div class="name">{{ value.word }}</div>
         <div class="spell">{{ value.spell }}</div>
         <div class="type">{{ value.lexical_category }}</div>
@@ -21,13 +29,20 @@
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import { mapActions, mapGetters } from "vuex";
-import { GridLarge, FormatListBulleted, PlayBoxOutline } from "mdue";
+
+import {
+  GridLarge,
+  FormatListBulleted,
+  PlayBoxOutline,
+  InformationVariant,
+} from "mdue";
 
 @Options({
   components: {
     GridLarge,
     FormatListBulleted,
     PlayBoxOutline,
+    InformationVariant,
   },
   data() {
     return {
@@ -40,6 +55,12 @@ import { GridLarge, FormatListBulleted, PlayBoxOutline } from "mdue";
     "$route.params.word": function (par) {
       this.currentRoute = par;
     },
+    getInfo: {
+      handler(val) {
+        console.log(val, "infoooo");
+      },
+      deep: true,
+    },
     getWord: {
       handler(val) {
         console.log(val);
@@ -51,11 +72,13 @@ import { GridLarge, FormatListBulleted, PlayBoxOutline } from "mdue";
   computed: {
     ...mapGetters({
       getWord: "AGENDA/getData",
+      getInfo: "TURENG/getData",
     }),
   },
   methods: {
     ...mapActions({
       actionAgenda: "AGENDA/AGENDA_GET",
+      actionInfo: "TURENG/WORD_INFO_REQUEST",
     }),
     getPronunciationsInfo(data: any) {
       if (typeof data.lexicalEntries[0]["entries"] !== "undefined") {
@@ -69,6 +92,9 @@ import { GridLarge, FormatListBulleted, PlayBoxOutline } from "mdue";
     },
     listViewChanger() {
       this.viewType = !this.viewType;
+    },
+    wordInfo(word: string) {
+      this.actionInfo(word);
     },
   },
   created() {
