187759cbe6bd4b6dfbb39e64998c227a6e218f3c
TARIK SARI <info@tariksari.com>
2020-11-25 00:48:26 +0300

    add word store

---
+++ src/_store/modules/word/actions.ts
@@ -0,0 +1,43 @@
+import Axios from '@/_utils/http-client';
+import {types} from '../../types';
+import * as CONSTANTS from './constants';
+
+export default {
+    [CONSTANTS.WORD_REQUEST]: ({commit, dispatch}: types, wordType: string) => {
+        Axios.get('service/word/' + wordType + '/all')
+            .then((resp) => {
+                commit(CONSTANTS.WORD_RESPONSE, resp);
+            })
+            .catch((err) => {
+                commit(CONSTANTS.WORD_ERROR, err.status);
+            });
+    },
+    [CONSTANTS.ADD_WORD_REQUEST]: ({commit, dispatch}: types, formData: object) => {
+        commit(CONSTANTS.ADD_WORD_BUTTON_STATUS, true);
+
+        Axios.post('service/word', formData)
+            .then((resp) => {
+                commit(CONSTANTS.ADD_WORD_RESPONSE, resp);
+                setTimeout(() => commit(CONSTANTS.ADD_WORD_BUTTON_STATUS, false), 8000);
+            })
+            .catch((err) => {
+                commit(CONSTANTS.ADD_WORD_ERROR, err.status);
+                setTimeout(() => commit(CONSTANTS.ADD_WORD_BUTTON_STATUS, false), 3000);
+            });
+    },
+    [CONSTANTS.UPDATE_WORD_REQUEST]: ({commit, dispatch}: types, formData: object) => {
+        Axios.put('service/word', formData)
+            .then((resp) => {})
+            .catch((err) => {
+                commit(CONSTANTS.UPDATE_WORD_ERROR, err.status);
+            });
+    },
+
+    [CONSTANTS.UPDATE_WORD_ORDER_REQUEST]: ({commit, dispatch}: types, formData: object) => {
+        Axios.put('service/word/order', formData)
+            .then((resp) => {})
+            .catch((err) => {
+                commit(CONSTANTS.UPDATE_WORD_ORDER_ERROR, err.status);
+            });
+    },
+};
---
+++ src/_store/modules/word/mutations.ts
@@ -0,0 +1,25 @@
+import * as CONSTANTS from './constants';
+
+export default {
+	[CONSTANTS.WORD_RESPONSE]: (state:any, data:string) => {
+		state.data = data;
+	},
+	[CONSTANTS.WORD_ERROR]: (state:any, err:string) => {
+		state.data = err;
+	},
+	[CONSTANTS.ADD_WORD_RESPONSE]: (state:any, data:string) => {
+		state.data = data;
+	},
+	[CONSTANTS.ADD_WORD_ERROR]: (state:any, err:string) => {
+		state.error = err;
+	},
+	[CONSTANTS.UPDATE_WORD_ERROR]: (state:any, err:string) => {
+		state.error = err;
+	},
+	[CONSTANTS.UPDATE_WORD_ORDER_ERROR]: (state:any, err:string) => {
+		state.error = err;
+	},
+	[CONSTANTS.ADD_WORD_BUTTON_STATUS]: (state: any, status: boolean) => {
+		state.buttonStatus = status;
+	},
+};
