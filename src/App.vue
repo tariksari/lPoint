01a7f1e1315988c9d9cbe9a87700045f24aad27c
TARIK SARI <info@tariksari.com>
2020-11-27 15:58:33 +0300

    fix: nav menu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
-  <div id="nav">
-    <router-link to="/">AGENDA</router-link>
-    <router-link to="/search">SEARCH WORD</router-link>
-  </div>
+  <nav class="shadow-2xl shadow-inner">
+    <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex items-center justify-between h-16">
+        <div class="flex items-center">
+          <div class="flex items-baseline">
+            <router-link
+              class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white"
+              to="/"
+              >AGENDA</router-link
+            >
+            <router-link
+              class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white"
+              to="/search"
+              >SEARCH WORD</router-link
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
   <load-bar></load-bar>
   <router-view />
 </template>
