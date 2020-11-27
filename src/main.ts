26b710427916158a2a185fea2164c0f041eb23da
TARIK SARI <info@tariksari.com>
2020-11-27 15:53:30 +0300

    add vue modal

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import App from './App.vue';
 import router from './router';
 import store from './_store';
 import MainDirective from './_directive'
+import VueFinalModal from 'vue-final-modal'
 
 const app = createApp(App);
 
 app.use(store);
 app.use(router);
 app.use(MainDirective);
+app.use(VueFinalModal())
 
 app.mount('#app');
